986874033c40f677d4ffc3c8bd4b14da504e5057
Paulo Bastos <paulosetembro10@hotmail.com>
2024-11-07 21:06:17 +0000

    Added back navigation and theme support, working on front page

--- main.js
+++ main.js
@@ -1,35 +1,56 @@
 let pageState = 'overview'
 
 window.onpopstate = function (event) {
-    if (event.state)
+    if (event.state !== null)
     {
         pageState = event.state;
-        changeContent(pageState);
+        changeContent(pageState, false);
     }
 }
 
 function initialize()
 {
-    window.history.replaceState(pageState, null, "");
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.setAttribute('data-bs-theme', 'dark');
+    }
+    else {
+        document.body.setAttribute('data-bs-theme', 'light');
+    }
+    
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+        if (e.matches) {
+            document.body.setAttribute('data-bs-theme', 'dark');
+        }
+        else {
+            document.body.setAttribute('data-bs-theme', 'light');
+        }
+    });
+
+    history.replaceState(pageState, null, "");
+    changeContent(pageState, false);
 }
 
-function changeContent(page)
+function changeContent(page, pushState = true)
 {
     var contentDiv = document.getElementById('content');
-
+    
     var xhr = new XMLHttpRequest();
     xhr.open('GET', `${page}.html`, true);
     xhr.onreadystatechange = function() {
         if (this.readyState !== 4)
             return;
         if (this.status !== 200)
-        {
-            contentDiv.innerHTML = `<h2>Content not found!</h2>`
-            return;
+            {
+                contentDiv.innerHTML = `<h2>Content not found!</h2>`
+                return;
         }
         contentDiv.innerHTML = this.responseText;
         pageState = page;
-        window.history.pushState(pageState, null, "");
+        if (pushState && history.state !== pageState) {
+            history.pushState(pageState, null, "");
+        }
     }
     xhr.send();
 }
+
+initialize();
