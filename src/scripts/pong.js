dd9943d015945249ff700746edb96a50999d058c
Paulo Bastos <paulosetembro10@hotmail.com>
2025-03-10 20:17:45 +0000

    Fix for "invisible" pong when using light theme

--- src/scripts/pong.js
+++ src/scripts/pong.js
@@ -14,6 +14,10 @@
 	const ballSize = 10;
 	const maxPoints = 5;
 
+	const paddle1Color = 'green';
+	const paddle2Color = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'white' : 'black';
+	const ballColor = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'white' : 'black';
+
 	let ball = { x: canvas.width / 2, y: canvas.height / 2, vx: BALL_SPEED || 4, vy: BALL_SPEED || 4, hits: 0, lastLoser: null };
 
 	let player1 = { x: 0, y: canvas.height / 2 - paddleHeight / 2, score: 0, up: false, down: false };
@@ -154,11 +158,11 @@
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 		// Draw paddles
-		drawRect(player1.x, player1.y, paddleWidth, paddleHeight, 'green');
-		drawRect(player2.x, player2.y, paddleWidth, paddleHeight, 'white');
+		drawRect(player1.x, player1.y, paddleWidth, paddleHeight, paddle1Color);
+		drawRect(player2.x, player2.y, paddleWidth, paddleHeight, paddle2Color);
 
 		// Draw ball
-		drawBall(ball.x, ball.y, ballSize, 'white');
+		drawBall(ball.x, ball.y, ballSize, ballColor);
 
 		// Draw scores
 		drawText(player1.score, canvas.width / 4, 50);
