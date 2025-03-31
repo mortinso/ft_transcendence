f8363c8416b016adc681ddf0c70ddf85bebf34a4
faleite <fabricio_2310@hotmail.com>
2025-03-31 19:57:00 +0100

    (Change) 42 API now on .env

--- docker/srcs/requirements/backend/tools/backend.sh
+++ docker/srcs/requirements/backend/tools/backend.sh
@@ -2,9 +2,6 @@
 
 cd transcendence
 
-export INTRA42_CLIENT_ID=$(cat $INTRA42_CLIENT_ID_FILE)
-export INTRA42_CLIENT_SECRET=$(cat $INTRA42_CLIENT_SECRET_FILE)
-
 if [ "$DEBUG" = "1" ]; then
     pip install -r requirements-dev.txt
 else
