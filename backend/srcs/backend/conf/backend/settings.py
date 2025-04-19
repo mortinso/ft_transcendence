9cbb3411ea02d370b762b4205ab0348be8511722
Pedro Santos <116544496+apedrocsantos@users.noreply.github.com>
2025-04-19 20:36:42 +0100

    Backend (#87)
    * protecting game endpoints
    
    * removed unused variables and views from user
    
    * protect game api endpoints
    
    * corrections to ELK
    
    * changed dashboard, activated password, disabled debug
    
    * going to merge with another commit
    
    ---------
    
    Co-authored-by: anda-cun <anda-cun@student.42lisboa.com>
    Co-authored-by: faleite <fabricio_2310@hotmail.com>

--- backend/srcs/backend/conf/backend/settings.py
+++ backend/srcs/backend/conf/backend/settings.py
@@ -203,33 +203,30 @@ DEBUG_TOOLBAR_CONFIG = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("POSTGRES_DB"),
-        'USER': env("POSTGRES_USER"),
-        'PASSWORD': env("POSTGRES_PASSWORD"),
+        'NAME': env("POSTGRES_DJANGO_DB"),
+        'USER': env("POSTGRES_DJANGO_USER"),
+        'PASSWORD': env("POSTGRES_DJANGO_PASSWORD"),
         'HOST': 'postgres',
         'PORT': '5432',
     }
 }
 
-# TODO: enable password validation
-# TODO: change password requirements
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    # },
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
 
@@ -300,8 +297,6 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = True
-
-# TODO: JWT settings
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
